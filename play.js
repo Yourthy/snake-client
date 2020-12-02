9f296425b632faa4461332565c492004dd0bae66
Yourthy <christopher.t.yourth@gmail.com>
2020-12-02 23:38:48 +0000

    added a user input function

--- play.js
+++ play.js
@@ -1,20 +1,29 @@
-const net = require('net');
+const { stdin } = require('process');
+const { StringDecoder } = require('string_decoder');
+const { connect } = require('./client');
+console.log('Connecting ...');
 
 /**
- * Establishes connection with the game server
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-});
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-    conn.on('data', (data) =>{
-        console.log(data);
-    });
-  return conn;
-}
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    
+    stdin.on('data', handleUserInput);
+    
+    return stdin;
+};
 
-console.log('Connecting ...');
+const handleUserInput = function(data) {
+    if(data === '\u0003'){
+        proccess.exit();
+    }
+};
+
+
+setupInput();
 connect();
