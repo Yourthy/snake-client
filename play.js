688ef65ca0708cb27b41bc37e486552a14843ab8
Yourthy <christopher.t.yourth@gmail.com>
2020-12-02 23:46:24 +0000

    removed the two functions and added reference to them

--- play.js
+++ play.js
@@ -1,29 +1,7 @@
-const { stdin } = require('process');
-const { StringDecoder } = require('string_decoder');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    
-    stdin.on('data', handleUserInput);
-    
-    return stdin;
-};
-
-const handleUserInput = function(data) {
-    if(data === '\u0003'){
-        proccess.exit();
-    }
-};
-
+connect();
 
 setupInput();
-connect();
